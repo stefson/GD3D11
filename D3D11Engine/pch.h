a37a082c180438a20e141695aacfc52830fab4bc
Kirides <john.heckendorf@outlook.com>
2019-12-27 17:52:19 +0100

    v17.7-dev8

--- D3D11Engine/pch.h
+++ D3D11Engine/pch.h
@@ -28,7 +28,7 @@
 #define stdext std
 #endif
 
-#define VERSION_NUMBER "17.7-dev7"
+#define VERSION_NUMBER "17.7-dev8"
 __declspec(selectany) const char* VERSION_NUMBER_STR = VERSION_NUMBER;
 
 extern bool GMPModeActive;
