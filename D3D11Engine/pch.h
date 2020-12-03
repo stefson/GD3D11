d9e90b8b953b224965f40f4ecd6d1b926316df98
Kirides <john.heckendorf@outlook.com>
2020-12-03 22:23:31 +0100

    dev15

--- D3D11Engine/pch.h
+++ D3D11Engine/pch.h
@@ -26,7 +26,7 @@
 #define stdext std
 #endif
 
-#define VERSION_NUMBER "17.7-dev14-fix3"
+#define VERSION_NUMBER "17.7-dev15"
 __declspec(selectany) const char* VERSION_NUMBER_STR = VERSION_NUMBER;
 
 extern bool GMPModeActive;
