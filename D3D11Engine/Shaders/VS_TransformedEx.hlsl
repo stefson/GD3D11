8dfa95b766642a7151b4738ebfa3e3b8764e2460
SaiyansKing <38609240+SaiyansKing@users.noreply.github.com>
2021-09-12 17:41:07 +0200

    Fixes
    -Fix broken fixed-function pipeline vertex shaders
    -Fix possible broken winding order when drawing HUD

--- D3D11Engine/Shaders/VS_TransformedEx.hlsl
+++ D3D11Engine/Shaders/VS_TransformedEx.hlsl
@@ -43,12 +43,12 @@ float4 TransformXYZRHW(float4 xyzrhw)
 	ndc.y = 1 - ((2 * (xyzrhw.y - V_ViewportPos.y)) / V_ViewportSize.y);
 	ndc.z = xyzrhw.z;
 	
+	// Remove the stupid half-pixel offset from pre D3D10
+	ndc.xy -= 0.5f / V_ViewportSize;
+	
 	// Convert to clip-space. rhw is actually 1/w ("reciprocal"). So to undo the devide by w, devide by the given 1/w.
 	float actualW = 1.0f / xyzrhw.w;
 	float3 clipSpace = ndc.xyz * actualW;
-	
-	// Remove the stupid half-pixel offset from pre D3D10
-	clipSpace.xy -= 0.5f / V_ViewportSize;
 	
 	return float4(clipSpace, actualW);
 		
