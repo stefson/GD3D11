741a7ea88725fbe4edde338e906ba983b83c3389
Kirides <john.heckendorf@outlook.com>
2021-12-14 13:39:38 +0100

    v17.8-dev1

--- D3D11Engine/pch.h
+++ D3D11Engine/pch.h
@@ -27,7 +27,7 @@
 #define stdext std
 #endif
 
-#define VERSION_NUMBER "17.7-dev34"
+#define VERSION_NUMBER "17.8-dev1"
 __declspec(selectany) const char* VERSION_NUMBER_STR = VERSION_NUMBER;
 
 extern bool FeatureLevel10Compatibility;
